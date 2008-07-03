fcf6b9146a85ce6d4545fabe176ac80ba9b706aa
heriniaina.eugene <heriniaina.eugene@23e6ee1f-784e-0410-b58e-b1cedd78a7fc>
2008-07-03 22:10:02 +0000

    Module admins controller is now ok

--- trunk/application/modules/admin/controllers/admins.php
+++ trunk/application/modules/admin/controllers/admins.php
@@ -1,6 +1,7 @@
 <?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
 	
 	class Admins extends Controller {
+		var $levels;
 		
 		function Admins()
 		{
@@ -10,6 +11,13 @@
 			$this->load->library('administration');
 			
 			$this->template['module'] = "admin";
+			$this->template['levels'] = array(
+					0 => __("No access"),
+					1 => __("Can view"),
+					2 => __("Can add"),
+					3 => __("Can edit"),
+					4 => __("Can delete")
+					);
 		}
 		
 		function index()
@@ -24,7 +32,7 @@
 		
 		function create()
 		{
-			$this->layout->load($this->template, 'admins/edit');
+			$this->layout->load($this->template, 'admins/create');
 		}
 		
 		function edit($id)
@@ -35,17 +43,43 @@
 			$this->layout->load($this->template, 'admins/edit');
 		}
 		
-		function save()
+		function save($id = null)
 		{
-			$data = array(
-				'username' => $this->input->post('username'),
-				'module' => $this->input->post('module'),
-				'level' => $this->input->post('level')
-				);
-			$this->data->insert('admins');
-			$this->session->set_flashdata('notification', __("Administrator list saved"));
-			redirect('admin/admins');
+			if ($this->input->post('submit'))
+			{
+				$this->db->where(array(
+					'username' => $this->input->post('username'),
+					'module' => $this->input->post('module'))
+					);
+				$query = $this->db->get('admins');
+				$data = array(
+					'username' => $this->input->post('username'),
+					'module' => $this->input->post('module'),
+					'level' => $this->input->post('level')
+					);
+				if ($query->num_rows() > 0)
+				{
+					$this->db->where(array(
+					'username' => $this->input->post('username'),
+					'module' => $this->input->post('module'))
+					);
+					$this->db->update('admins', $data);
+					$this->session->set_flashdata('notification', __("Administrator list updated"));
+		
+				}
+				else
+				{
+					$this->db->insert('admins', $data);	
+					$this->session->set_flashdata('notification', __("Administrator added in list"));
+				}
+			}
+			else
+			{
+				$this->session->set_flashdata('notification', __("Nothing to save"));
+			}
+			redirect('admin/admins');			
 		}
+		
 		function delete($id)
 		{
 			$this->db->where('id', $id);
