c089e4db39015ddf0da458d09cc37230af75a2ff
heriniaina.eugene <heriniaina.eugene@23e6ee1f-784e-0410-b58e-b1cedd78a7fc>
2008-07-20 23:06:11 +0000

    + Using pagebreak ! Images from page module only ! other bug fixed

--- trunk/application/modules/page/page_blocks.php
+++ trunk/application/modules/page/page_blocks.php
@@ -8,7 +8,7 @@ function page_latest_pages($limit = 5)
 	
 	$this->obj->load->model('page_model');
 	$data['pages'] = $this->obj->page_model->new_pages($limit);
-	echo $this->obj->load->view('blocks/newpages', $data, true);
+	return $this->obj->load->view('blocks/newpages', $data, true);
 }
 
 ?>
--- trunk/application/modules/page/views/create.php
+++ trunk/application/modules/page/views/create.php
@@ -80,7 +80,7 @@ function ajaxFileUpload() {
 						alert(data.error);
 					}else
 					{
-						$("#image_list").append("<div><input type='hidden' name='image_ids[]' value='"+data.imageid+"' /><a href='#' onclick=\"tinyMCE.execCommand('mceInsertContent',false,'<a href=\\'<?php echo site_url('images/o') ?>/"+data.image+"\\'><img border=0 align=left hspace=10 src=\\'<?php echo site_url('images/m') ?>/"+data.image+"\\'></a>');return false;\">"+data.image+"</a> - <a href='#'  class=\"ajaxdelete\" id='image"+data.imageid+"' ><?php echo __('Delete image')?></a></div>\n");
+						$("#image_list").append("<div><input type='hidden' name='image_ids[]' value='"+data.imageid+"' /><a href='#' onclick=\"tinyMCE.execCommand('mceInsertContent',false,'<a href=\\'<?php echo site_url('images/o') ?>/"+data.image+"\\'><img border=0 align=left hspace=10 src=\\'<?php echo site_url('images/m') ?>/"+data.image+"\\'></a>');return false;\">"+data.image+"</a> - <a href='#'  class=\"ajaxdelete\" id='"+data.imageid+"' ><?php echo __('Delete image')?></a></div>\n");
 						$("#image").val("");
 						handleDeleteImage();
 						
