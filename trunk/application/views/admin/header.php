6ca58f87c6d5a925332b771417678ea4b999e477
heriniaina.eugene <heriniaina.eugene@23e6ee1f-784e-0410-b58e-b1cedd78a7fc>
2008-07-20 23:06:47 +0000

    + Using pagebreak plugin in tinymce

--- trunk/application/views/admin/header.php
+++ trunk/application/views/admin/header.php
@@ -20,8 +20,8 @@
 		theme : "advanced",
 		plugins : "safari,pagebreak,style,layer,table,advhr,advimage,advlink,inlinepopups,insertdatetime,xhtmlxtras",
 		languages : "en",
-		theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,bullist,numlist,|,link,unlink,|,formatselect,|,undo,redo,insertdate,inserttime,image,cleanup,code",
-		theme_advanced_buttons2 : "",
+		theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,bullist,numlist,|,link,unlink,|,fontselect,formatselect,|,undo,redo,image",
+		theme_advanced_buttons2 : "insertlayer,|,tablecontrols,|,forecolor,backcolor,insertdate,inserttime,pagebreak,|,cleanup,removeformat,code",
 		theme_advanced_buttons3 : "",
 		theme_advanced_toolbar_location : "top",
 		theme_advanced_toolbar_align : "left",
@@ -31,6 +31,7 @@
 		entity_encoding : "raw",
 		fix_list_elements : true,
 		relative_urls : false,
+		pagebreak_separator : "<!-- page break -->",
 		extended_valid_elements : "a[name|href|target|title|onclick],img[class|src|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name],hr[class|width|size|noshade],font[face|size|color|style],span[class|align|style]"
 	});
 	</script>
