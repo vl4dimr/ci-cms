01544c9bfe19e9614aa5669b9fc8b5b8753ba746
heriniaina.eugene <heriniaina.eugene@23e6ee1f-784e-0410-b58e-b1cedd78a7fc>
2008-07-25 13:06:29 +0000

    -

--- dev/modules/news/views/read.php
+++ dev/modules/news/views/read.php
@@ -23,24 +23,24 @@
 <h2><?=__("Add a comment")?></h2>
 <form action="<?=site_url('news/comment')?>" method='post'>
 <input type='hidden' name='news_id' value='<?=$news['id']?>' />
-<input type='hidden' name='uri' value='<?=$news['uri']?>' />
+<input  class="input-text" type='hidden' name='uri' value='<?=$news['uri']?>' />
 <label for='author'><?=__("Name:")?>[*]</label>
-<input type='text' name='author' value='' id='name' /><br />
+<input  class="input-text" type='text' name='author' value='' id='name' /><br />
 
 <label for='email'><?=__("Email:")?>[*]</label>
-<input type='text' name='email' value='' id='email' /><br />
+<input  class="input-text" type='text' name='email' value='' id='email' /><br />
 
 <label for='website'><?=__("Website:")?></label>
 <input type='text' name='website' value='' id='website' /><br />
 
 <label for='body'><?=__("Comment")?>[*]</label>
-<textarea name='body' id='body' /></textarea><br />
+<textarea  class="input-textarea" name='body' id='body' rows="10" /></textarea><br />
 
 <label><?=__("Security code:")?></label><?=$captcha?><br />
 <label for="captcha"><?=__("Confirm security code:")?></label>
-<input type='text' name='captcha' value='' /><br />
+<input class="input-text" type='text' name='captcha' value='' /><br />
 [*] <?=__("Required")?><br />
-<input type='submit' name='submit' value="<?=__("Add comment")?>" /><br />
+<input type='submit' name='submit' class="input-submit" value="<?=__("Add comment")?>" /><br />
 </form>
 </div>
 <? endif ;?>
