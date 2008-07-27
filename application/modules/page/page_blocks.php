14ed87c00eb0cdd14604da6177cc65f0ab8bfe91
heriniaina.eugene <heriniaina.eugene@23e6ee1f-784e-0410-b58e-b1cedd78a7fc>
2008-07-27 15:01:12 +0000

    ! The $this problem had an error with php5

--- application/modules/page/page_blocks.php
+++ application/modules/page/page_blocks.php
@@ -4,11 +4,11 @@ $this->set('page_latest_pages', 'page_latest_pages');
 		
 function page_latest_pages($limit = 5)
 {
-	$this->obj =& get_instance();
+	$obj =& get_instance();
 	
-	$this->obj->load->model('page_model');
-	$data['pages'] = $this->obj->page_model->new_pages($limit);
-	return $this->obj->load->view('blocks/newpages', $data, true);
+	$obj->load->model('page_model');
+	$data['pages'] = $obj->page_model->new_pages($limit);
+	return $obj->load->view('blocks/newpages', $data, true);
 }
 
 ?>
