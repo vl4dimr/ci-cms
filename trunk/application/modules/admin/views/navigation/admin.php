04430fd69d8c0951c3da1d7b3ab419915e959367
heriniaina.eugene <heriniaina.eugene@23e6ee1f-784e-0410-b58e-b1cedd78a7fc>
2008-08-31 22:36:50 +0000

    ! Fixed some language Translation

--- trunk/application/modules/admin/views/navigation/admin.php
+++ trunk/application/modules/admin/views/navigation/admin.php
@@ -1,11 +1,11 @@
 <!-- [Content] start -->
 <div class="content wide">
 
-<h1 id="navigation"><?=__("Navigation administration", $this->template['module'])?></h1>
+<h1 id="navigation"><?=__("Navigation administration", 'admin')?></h1>
 
 <ul class="manage">
-	<li><a href="<?=site_url('admin/navigation/create')?>" class="first"><?=__("Create new", $this->template['module'])?></a></li>
-	<li><a href="<?=site_url('admin')?>" class="last"><?=__("Cancel", $this->template['module'])?></a></li>
+	<li><a href="<?=site_url('admin/navigation/create')?>" class="first"><?=__("Create new", 'admin')?></a></li>
+	<li><a href="<?=site_url('admin')?>" class="last"><?=__("Cancel", 'admin')?></a></li>
 </ul>
 		
 <br class="clearfloat" />
@@ -20,9 +20,9 @@
 	<thead>
 		<tr>
 				<th width="3%" class="center">#</th>
-				<th width="27%"><?=__("Menu", $this->template['module'])?></th>
-				<th width="40%"><?=__("Target", $this->template['module'])?></th>
-				<th width="30%" colspan="3"><?=__("Action", $this->template['module'])?></th>
+				<th width="27%"><?=__("Menu", 'admin')?></th>
+				<th width="40%"><?=__("Target", 'admin')?></th>
+				<th width="30%" colspan="3"><?=__("Action", 'admin')?></th>
 		</tr>
 	</thead>
 	<tbody>
@@ -35,15 +35,15 @@
 				<td><?=($nav['level'] > 0) ? "|".str_repeat("__", $nav['level']): ""?> <?=$nav['title']?></td>
 				<td><?=$nav['uri']?></td>
 				<td>
-				<a href="<?=site_url('admin/navigation/move/up/'. $nav['id'])?>"><img src="<?=site_url('application/views/admin/images/moveup.gif')?>" width="16" height="16" title="<?=__("Move up", $this->template['module'])?>" alt="<?=__("Move up", $this->template['module'])?>"/></a>
-				<a href="<?=site_url('admin/navigation/move/down/'. $nav['id'])?>"><img src="<?=site_url('application/views/admin/images/movedown.gif')?>" width="16" height="16" title="<?=__("Move down", $this->template['module'])?>" alt="<?=__("Move down", $this->template['module'])?>"/></a></td>
-				<td><a href="<?=site_url('admin/navigation/edit/'.$nav['id'])?>"><?=__("Edit", $this->template['module'])?></a></td>
-				<td><a href="<?=site_url('admin/navigation/delete/'.$nav['id'])?>"><?=__("Delete", $this->template['module'])?></a></td>
+				<a href="<?=site_url('admin/navigation/move/up/'. $nav['id'])?>"><img src="<?=site_url('application/views/admin/images/moveup.gif')?>" width="16" height="16" title="<?=__("Move up", 'admin')?>" alt="<?=__("Move up", 'admin')?>"/></a>
+				<a href="<?=site_url('admin/navigation/move/down/'. $nav['id'])?>"><img src="<?=site_url('application/views/admin/images/movedown.gif')?>" width="16" height="16" title="<?=__("Move down", 'admin')?>" alt="<?=__("Move down", 'admin')?>"/></a></td>
+				<td><a href="<?=site_url('admin/navigation/edit/'.$nav['id'])?>"><?=__("Edit", 'admin')?></a></td>
+				<td><a href="<?=site_url('admin/navigation/delete/'.$nav['id'])?>"><?=__("Delete", 'admin')?></a></td>
 		</tr>
 <?php $i++; endforeach;?>
 <?php endif; ?>
 	</tbody>
 </table>
-<?=//$pager?>
+
 </div>
 <!-- [Content] end -->
