2b3edb5e8b24373bc7b21b2f9ecc85e70ba7a2f9
heriniaina.eugene <heriniaina.eugene@23e6ee1f-784e-0410-b58e-b1cedd78a7fc>
2008-10-13 05:21:43 +0000

    + Search module... to do

---
+++ application/modules/search/controllers/search.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * $Id$
+ *
+ *
+ */
+  
+
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Search extends Controller {
+
+	function Search()
+	{
+		parent::Controller();
+		$this->template['module'] = 'search';
+
+	}
+	
+	
+	function index()
+	{
+		
+	}
+	
+	function result()
+	{
+	
+	}
+}	
