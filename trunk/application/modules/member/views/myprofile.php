fb434299654aaaec413a370ce3ede82dfbfc8f87
heriniaina.eugene <heriniaina.eugene@23e6ee1f-784e-0410-b58e-b1cedd78a7fc>
2008-11-22 16:02:01 +0000

    * Member's profile is now ok

--- trunk/application/modules/member/views/myprofile.php
+++ trunk/application/modules/member/views/myprofile.php
@@ -6,7 +6,14 @@
 <form class="settings" action="<?=site_url('member/profile')?>" method="post" accept-charset="utf-8">
 		<br class="clearfloat" />
 
-		<?php echo $this->validation->error_string; ?>
+			<?php echo $this->validation->error_string; ?>
+			<br />
+			
+			<?php 
+			
+			$msg = __("Please fill here your profile. Do not touch those you don't want to change.", $this->template['module']);
+			echo $this->plugin->apply_filters('member_profile_pre_form', $msg);
+			?>
 			<br />
 			<label for="password"><?=__("Password", $this->template['module'])?>:</label>
 			<input type="password" name="password" value="" id="password" class="input-text" /><br />
@@ -14,7 +21,16 @@
 			<label for="passconf"><?=__("Confirm", $this->template['module'])?>:</label>
 			<input type="password" name="passconf" value="" id="" class="input-text" /><br />			
 			<label for="email"><?=__("Email", $this->template['module'])?>:</label>
-			<input type="text" name="email" value="<?=$member['email']?>" id="email" class="input-text" /><br />
+			<input type="text" name="email" value="<?php echo $member['email']?>" id="email" class="input-text" /><br />
 			
+			<?php 
+			
+			$msg = "";
+			echo $this->plugin->apply_filters('member_profile_post_form', $msg);
+			?>
+			<input type="submit" name="submit" value="<?php echo __("Save", $this->template['module'])?>" class="input-submit" />
+			<a href="<?php echo site_url( $this->session->userdata("last_uri") )?>" class="input-submit"><?php echo __("Cancel", $this->template['module'])?></a>
+
+</form>
 
 <!-- [Content] end -->
