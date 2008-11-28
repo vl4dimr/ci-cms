59492d0ba016e8098d9d17053d87f8108b5d89af
heriniaina.eugene <heriniaina.eugene@23e6ee1f-784e-0410-b58e-b1cedd78a7fc>
2008-11-28 17:04:24 +0000

    + adding save_page action that can be tiggered

--- trunk/application/modules/page/views/create.php
+++ trunk/application/modules/page/views/create.php
@@ -1,12 +1,12 @@
 <!-- [Left menu] start -->
 <div class="leftmenu">
 
-	<h1 id="pageinfo">Page informations</h1>
+	<h1 id="pageinfo"><?php _e("Page informations", $this->template['module']) ?></h1>
 	
 	<ul id="tabs" class="quickmenu">
-		<li><a href="#one"><?=__("Content", $this->template['module'])?></a></li>
-		<li><a href="#two"><?=__("Meta data", $this->template['module'])?></a></li>
-		<li><a href="#three"><?=__("Options", $this->template['module'])?></a></li>
+		<li><a href="#one"><?php echo __("Content", $this->template['module'])?></a></li>
+		<li><a href="#two"><?php echo __("Other fields", $this->template['module'])?></a></li>
+		<li><a href="#three"><?php echo __("Options", $this->template['module'])?></a></li>
 	</ul>
 	<div class="quickend"></div>
 
@@ -98,13 +98,13 @@ function ajaxFileUpload() {
 }
 </script>
 
-<h1 id="edit"><?=__("Create New Page", $this->template['module'])?></h1>
+<h1 id="edit"><?php echo __("Create New Page", $this->template['module'])?></h1>
 
-<form  enctype="multipart/form-data" class="edit" action="<?=site_url('admin/page/create')?>" method="post" accept-charset="utf-8">
+<form  enctype="multipart/form-data" class="edit" action="<?php echo site_url('admin/page/create')?>" method="post" accept-charset="utf-8">
 		<input type="hidden" name="lang" value="<?php echo $this->user->lang ?>" />
 		<ul>
 			<li><input type="submit" name="submit" value="Save page" class="input-submit" /></li>
-			<li><a href="<?=site_url('admin/page')?>" class="input-submit last">Cancel</a></li>
+			<li><a href="<?php echo site_url('admin/page')?>" class="input-submit last">Cancel</a></li>
 		</ul>
 		
 		<br class="clearfloat" />
@@ -112,45 +112,45 @@ function ajaxFileUpload() {
 		<hr />
 
 		<?php if ($notice = $this->session->flashdata('notification')):?>
-		<p class="notice"><?=$notice;?></p>
+		<p class="notice"><?php echo $notice;?></p>
 		<?php endif;?>
 		
 		<div id="one">
 		
-		<p><?=__("To create a new page, just fill in your content below and click 'Save page'.<br />If you want to save your progress without publishing the page, Select 'Draft' status.", $this->template['module'])?></p>
+		<p><?php echo __("To create a new page, just fill in your content below and click 'Save page'.<br />If you want to save your progress without publishing the page, Select 'Draft' status.", $this->template['module'])?></p>
 
-		<label for="title"><?=__("Page Title", $this->template['module'])?>:</label>
+		<label for="title"><?php echo __("Page Title", $this->template['module'])?>:</label>
 		<input type="text" name="title" value="" id="title" class="input-text" /><br />
 		
 
 		
-		<label for="uri"><?=__("SEF address", $this->template['module'])?>:</label>
+		<label for="uri"><?php echo __("SEF address", $this->template['module'])?>:</label>
 		<input type="text" name="uri" value="" id="uri" class="input-text" /><br />
 		
-		<label for="parent_id"><?=__("Parent", $this->template['module'])?>: </label>
+		<label for="parent_id"><?php _e("Parent", $this->template['module'])?>: </label>
 		<select name="parent_id" class="input-select" />
 		<option value='0'/>
 		<?php if ($parents = $this->pages->list_pages()) : ?>
 		<?php foreach ($parents as $parent):?>
 
-		<option value="<?=$parent['id']?>"><?=($parent['level'] > 0) ? "|".str_repeat("__", $parent['level']): ""?> <?=$parent['title'] ?> </option>
+		<option value="<?php echo $parent['id']?>"><?php echo ($parent['level'] > 0) ? "|".str_repeat("__", $parent['level']): ""?> <?php echo $parent['title'] ?> </option>
 		
 		<?php endforeach;?>
 		<?php endif; ?>
 		</select>
 		<br />
 			
-		<label for="status">Status:</label>
+		<label for="status"><?php _e("Status:", $this->template['module'])?></label>
 		<select name="status" id="status" class="input-select">
-			<option value="0">Draft</option>
-			<option value="1" selected>Published</option>
+			<option value="0"><?php _e("Draft", $this->template['module'])?></option>
+			<option value="1" selected><?php _e("Published", $this->template['module'])?></option>
 		</select><br />
 		
-		<label for="body">Page Content:</label>
+		<label for="body"><?php _e("Page Content:", $this->template['module'])?></label>
 		<textarea name="body" class="input-textarea"></textarea><br />
 
 		<div id='image_list'>
-		<div style="visibility: hidden">Available images:</div>
+		<div style="visibility: hidden"><?php _e("Available images:", $this->template['module'])?></div>
 		<?php if ($images) : ?>
 		<?php foreach($images as $image): ?>
 		<div><input type='hidden' name='image_ids[]' value='<?php echo $image['id'] ?>' /><a href='#' onclick="tinyMCE.execCommand('mceInsertContent',false,'<a href=\'<?php echo site_url('media/images/o')?>/<?php echo $image['file'] ?>\'><img border=\'0\' align=\'left\' hspace=\'10\' src=\'<?php echo site_url('media/images/m')?>/<?php echo $image['file'] ?>\' /></a>');return false;"><?php echo $image['file'] ?></a> - <a href="<?php echo site_url('admin/page/removeimg/' . $image['id']) ?>" class="ajaxdelete" id="<?php echo $image['id'] ?>"><?php echo __("Delete image", $this->template['module']) ?></a></div>
@@ -158,30 +158,36 @@ function ajaxFileUpload() {
 		<?php endif;?>
 		</div>
 		
-		<label for="image"><?=__("Image", $this->template['module'])?></label>
+		<label for="image"><?php echo __("Image", $this->template['module'])?></label>
 		<input type="file" name="image" class="input-file" id="image"/><br />
 		</div>
 		<div id="two">
 		
-			<label for="meta_keywords">Page keywords:</label>
+			<label for="meta_keywords"><?php _e("Page keywords:", $this->template['module'])?></label>
 			<input type="text" name="meta_keywords" value="" id="meta_keywords" class="input-text" /><br />
 		
-			<label for="meta_description">Page description:</label>
-			<input type="text" name="meta_description" value="" id="meta_description" class="input-text" />
+			<label for="meta_description"><?php _e("Page description:", $this->template['module'])?></label>
+			<input type="text" name="meta_description" value="" id="meta_description" class="input-text" /><br />
 			
+			<?php
+			$custom_fields = "";
+			
+			echo $this->plugin->apply_filters("page_custom_fields", $custom_fields);
+
+			?>
 		</div>
 		<div id="three">
 		
-			<label for="options[show_subpages]"><?=__("Show subpages", $this->template['module'])?>:</label>
+			<label for="options[show_subpages]"><?php _e("Show subpages", $this->template['module'])?>:</label>
 			<select name="options[show_subpages]" class="input-select" id="show_subpages">
-			<option value='0' <?=((isset($row['options']['show_subpages']) && $row['options']['show_subpages']==0)?"selected":"")?>><?=__("No", $this->template['module'])?></option>
-			<option value='1' <?=((isset($row['options']['show_subpages']) && $row['options']['show_subpages']==1)?"selected":"")?>><?=__("Yes", $this->template['module'])?></option>
+			<option value='0' <?php echo ((isset($row['options']['show_subpages']) && $row['options']['show_subpages']==0)?"selected":"")?>><?php echo __("No", $this->template['module'])?></option>
+			<option value='1' <?php echo ((isset($row['options']['show_subpages']) && $row['options']['show_subpages']==1)?"selected":"")?>><?php echo __("Yes", $this->template['module'])?></option>
 			</select><br />
 			
-			<label for="options[show_navigation]"><?=__("Show navigation", $this->template['module'])?>:</label>
+			<label for="options[show_navigation]"><?php echo __("Show navigation", $this->template['module'])?>:</label>
 			<select name="options[show_navigation]" class="input-select" id="show_navigation">
-			<option value='0' <?=((isset($row['options']['show_navigation']) && $row['options']['show_navigation']==0)?"selected":"")?>><?=__("No", $this->template['module'])?></option>
-			<option value='1' <?=((isset($row['options']['show_navigation']) && $row['options']['show_navigation']==1)?"selected":"")?>><?=__("Yes", $this->template['module'])?></option>
+			<option value='0' <?php echo ((isset($row['options']['show_navigation']) && $row['options']['show_navigation']==0)?"selected":"")?>><?php echo __("No", $this->template['module'])?></option>
+			<option value='1' <?php echo ((isset($row['options']['show_navigation']) && $row['options']['show_navigation']==1)?"selected":"")?>><?php echo __("Yes", $this->template['module'])?></option>
 			</select><br />
 			
 		</div>
