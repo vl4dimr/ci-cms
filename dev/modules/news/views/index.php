c1fedd4c2d6bf459104a8167a5d503677c6e9de1
heriniaina.eugene <heriniaina.eugene@23e6ee1f-784e-0410-b58e-b1cedd78a7fc>
2008-12-20 06:46:18 +0000

    * News list style

--- dev/modules/news/views/index.php
+++ dev/modules/news/views/index.php
@@ -1,29 +1,19 @@
 <h1><?=__("All news", $this->template['module'])?></h1>
 <?php if (is_array($rows) && count($rows) > 0) :?>
-<table class="list" width='100%'>
-	<thead>
-		<tr>
-				<th width="3%" class="center">#</th>
-				<th width="27%"><?=__("Title", $this->template['module'])?></th>
-				<th width="70%"><?=__("Summary", $this->template['module'])?></th>
-		</tr>
-	</thead>
-	<tbody>
 <?php $i = 1; foreach ($rows as $row): ?>
 <?php 
 $page_break_pos = strpos($row['body'], "<!-- page break -->");
 $row['summary'] = substr($row['body'], 0, $page_break_pos);
 ?>		
 <?php if ($i % 2 != 0): $rowClass = 'odd'; else: $rowClass = 'even'; endif;?>
-		<tr class="<?=$rowClass?>" valign="top">
-				<td class="center"><?=$i?></td>
-				<td valign="top"><a href="<?=site_url('news/' . $row['uri'])?>"><?=$row['title']?></a></td>
-				<td valign="top"><?=(strlen(strip_tags($row['summary'])) >= 300 ? substr(strip_tags($row['summary']), 0,300) . '...': strip_tags($row['summary']))?></td>
-		</tr>
+<div class="<?=$rowClass?>">
+<h2><?=$i?> <?=$row['title']?></h2>
+<?=(strlen(strip_tags($row['summary'])) >= 300 ? substr(strip_tags($row['summary']), 0,300) . '...': strip_tags($row['summary']))?> <a href="<?=site_url('news/' . $row['uri'])?>">(<?php echo  __("More", $this->template['module']) ?>...)</a>
 <?php $i++; endforeach;?>
-	</tbody>
-</table>
+<div class="pager">
 <?=$pager?>
+</div>
+</div>
 <?php else : ?>
 <?=__("No news found", $this->template['module'])?>
 <?php endif; ?>
