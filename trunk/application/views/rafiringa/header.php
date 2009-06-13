aacb12b9b445ea44a6f1972a78e9e69a2f908b9c
heriniaina.eugene <heriniaina.eugene@23e6ee1f-784e-0410-b58e-b1cedd78a7fc>
2009-06-13 06:36:12 +0000

    + Lightbox added to rafiringa.net

--- trunk/application/views/rafiringa/header.php
+++ trunk/application/views/rafiringa/header.php
@@ -7,15 +7,16 @@
 	<meta name="description" content="<?php if (!empty($meta_description)):?><?php echo $meta_description?> - <?php endif; ?><?php echo $this->system->meta_description;?>" />
 	<meta name="robots" content="index,follow" />
 	<link rel="shortcut icon" href="<?=base_url()?>application/views/<?php echo $this->system->theme ?>/images/favicon.ico" type="image/x-icon" />
-	<link rel="alternate" type="text/xml" title="RSS 2.0" href="http://feeds.feedburner.com/rvg" />
+	<link rel="alternate" type="text/xml" title="RSS 2.0" href="http://rafiringa.net/feed" />
 	<link rel="stylesheet" href="<?=base_url()?>application/views/<?php echo $this->system->theme ?>/style/stylesheet.css" type="text/css" media="screen" charset="utf-8" />
+	<link rel="stylesheet" href="<?=base_url()?>application/views/<?php echo $this->system->theme ?>/style/jquery.lightbox-0.5.css" type="text/css" media="screen" charset="utf-8" />
 	<!--[if IE]>
 		<link rel="stylesheet" href="<?=base_url()?>application/views/<?php echo $this->system->theme ?>/style/iefix.css" type="text/css" media="screen" charset="utf-8" />
 	<![endif]-->
 	<script src="<?=base_url()?>application/views/<?php echo $this->system->theme ?>/javascript/jquery.js" type="text/javascript"></script>
 	<script src="<?=base_url()?>application/views/<?php echo $this->system->theme ?>/javascript/sitelib.js" type="text/javascript"></script>
 	<script src="<?=base_url()?>application/views/<?php echo $this->system->theme ?>/javascript/external.js" type="text/javascript"></script>
-	<script src="<?=base_url()?>application/views/<?php echo $this->system->theme ?>/javascript/superfish.js" type="text/javascript"></script>
+	<script src="<?=base_url()?>application/views/<?php echo $this->system->theme ?>/javascript/jquery.lightbox-0.5.pack.js" type="text/javascript"></script>
 
 <?php $this->plugin->do_action('header');?>
 <script type="text/javascript" src="http://partner.googleadservices.com/gampad/google_service.js">
