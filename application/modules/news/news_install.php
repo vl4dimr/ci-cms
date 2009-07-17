990d8231b9285ab71497d5381ae28b58ee48d295
heriniaina.eugene <heriniaina.eugene@23e6ee1f-784e-0410-b58e-b1cedd78a7fc>
2009-07-17 16:27:29 +0000

    ! Prefix error when installing... anyway this will be installed with CI-CMS as default

--- application/modules/news/news_install.php
+++ application/modules/news/news_install.php
@@ -16,5 +16,12 @@ $news_settings = serialize(array(
 	
 $this->system->set('news_settings', $news_settings);
 
+$this->db->query("CREATE TABLE IF NOT EXISTS " . $this->db->dbprefix('news') . " ( `id` INT NOT NULL AUTO_INCREMENT , `cat` INT NOT NULL DEFAULT '0', `title` VARCHAR( 255 ) NOT NULL , `uri` VARCHAR( 255 ) NOT NULL , `lang` VARCHAR( 255 ) NOT NULL , `body` TEXT NOT NULL , `allow_comments` tinyint(1) NOT NULL DEFAULT '1', `comments` int(4) NOT NULL, `status` INT(3) NOT NULL DEFAULT '0', `date` INT NOT NULL , `author`VARCHAR( 255 ) NOT NULL , `email` VARCHAR( 255 ) NOT NULL , `notify` TINYINT NOT NULL , `hit` INT(11) NOT NULL DEFAULT '0', `ordering` INT(11) NOT NULL DEFAULT '0', PRIMARY KEY ( `id` ) , INDEX ( `title` ) )"); 
 
-?>
+$this->db->query("CREATE TABLE IF NOT EXISTS " . $this->db->dbprefix('news_comments') . " (   `id` int(11) NOT NULL auto_increment,   `news_id` int(11) NOT NULL,   `status` int(2) NOT NULL DEFAULT '0',   `date` int(11) NOT NULL,   `author` varchar(50) NOT NULL,   `email` varchar(100) NOT NULL,   `website` varchar(150) NOT NULL,   `body` text NOT NULL,   `ip` varchar(150) NOT NULL,     PRIMARY KEY  (`id`),   KEY `news_id` (`news_id`),   KEY `date` (`date`),   KEY `status` (`status`) )");
+
+$this->db->query("CREATE TABLE IF NOT EXISTS " . $this->db->dbprefix('captcha') . " ( captcha_id bigint( 13 ) unsigned NOT NULL AUTO_INCREMENT , captcha_time int( 10 ) unsigned NOT NULL , ip_address varchar( 16 ) default '0' NOT NULL , word varchar( 20 ) NOT NULL , PRIMARY KEY ( captcha_id ) , KEY ( word ) )");
+
+$this->db->query("CREATE TABLE IF NOT EXISTS " . $this->db->dbprefix('news_cat') . "  ( `id` int(11) NOT NULL auto_increment, `pid` int(11) NOT NULL default '0', `title` varchar(255) NOT NULL default '', `icon` varchar(255) NOT NULL default '', `desc` text NOT NULL, `date` int(11) NOT NULL default '0', `username` varchar(20) NOT NULL default '', `lang` char(5) NOT NULL default '', `weight` int(11) NOT NULL default '0', `status` int(5) NOT NULL default '1', `acces` varchar(20) NOT NULL default '0', PRIMARY KEY  (`id`), KEY `title` (`title`) )");
+
+
