21197b0cf2bd728076994c3c955f810179a9172b
heriniaina.eugene <heriniaina.eugene@23e6ee1f-784e-0410-b58e-b1cedd78a7fc>
2009-07-17 22:41:09 +0000

    Ready for beta

--- application/views/default/footer.php
+++ application/views/default/footer.php
@@ -1,15 +1,8 @@
-			</td>
-			</tr>
-		</tbody>
-		</table>
-<div class='clear'>&nbsp;</div>
+<div id="copyright">
+Powered by <a href="http://ci-cms.googlecode.com">CI-CMS</a> | 
+Page rendered in {elapsed_time} seconds
 </div>
 <div id="footer">
-<div id="footer-inner">
-Concept and Design &copy; Serasera.org: <a href="http://hery.serasera.org">Eugene Hery</a> <img src="http://serasera.org/images/seraserapowered.gif" width="80" height="15" /> Tel +39 338 744 2412 (Italie)
-<br />Page rendered in {elapsed_time} seconds</p>
 </div>
-</div><!-- [Base] end -->
-
 </body>
 </html>
