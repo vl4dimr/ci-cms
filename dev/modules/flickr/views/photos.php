1a8e05aecc7d191f2d126de1cab673fafa66d4d1
heriniaina.eugene <heriniaina.eugene@23e6ee1f-784e-0410-b58e-b1cedd78a7fc>
2009-09-06 23:19:13 +0000

    So that it can be copied easily

--- dev/modules/flickr/views/photos.php
+++ dev/modules/flickr/views/photos.php
@@ -1,10 +1,8 @@
 <h1><?php echo $title ?></h1>
 <div class="flickr-album">
 <?php foreach ($photos as $photo) : ?>				
-<div class="flickr-image">
 <a href='http://static.flickr.com/<?php echo $photo['server'] ?>/<?php echo $photo['id'] ;?>_<?php echo $photo['secret']?>.jpg' title='<?php echo $photo['title']?>'>
 <img alt='<?php echo $photo['title'] ?>' src='http://static.flickr.com/<?php echo $photo['server'] ?>/<?php echo $photo['id'] ;?>_<?php echo $photo['secret']?>_s.jpg' />
 </a>
-</div>				       					
 <?php endforeach ; ?>
 </div>
