a06bd8ab2fae3fb41ff3e9630a7b410aecff87a3
heriniaina.eugene <heriniaina.eugene@23e6ee1f-784e-0410-b58e-b1cedd78a7fc>
2009-09-28 14:30:24 +0000

    * In manage categories, category uri is more evident

--- application/modules/news/views/admin/category.php
+++ application/modules/news/views/admin/category.php
@@ -21,7 +21,7 @@
 		<tr>
 				<th width="3%" class="center">#</th>
 				<th width="27%"><?=__("Name", $this->template['module'])?></th>
-				<th width="27%"><?=__("Summary", $this->template['module'])?></th>
+				<th width="27%"><?=__("URI", $this->template['module'])?></th>
 				<th width="15%"><?=__("Status", $this->template['module'])?></th>
 				<th width="5%"><?=__("Ordering", $this->template['module'])?></th>
 				<th width="20%" colspan="2"><?=__("Action", $this->template['module'])?></th>
@@ -31,22 +31,12 @@
 	<tbody>
 <?php if ($rows) : ?>
 <?php $i = 1; foreach ($rows as $row): ?>
-<?php 
-if($page_break_pos = strpos($row['desc'], "<!-- page break -->"))
-{
-	$row['summary'] = substr($row['desc'], 0, $page_break_pos);
-}
-else
-{
-	$row['summary'] = $row['desc'];
-}
-?>		
 
 <?php if ($i % 2 != 0): $rowClass = 'odd'; else: $rowClass = 'even'; endif;?>
 		<tr class="<?=$rowClass?>">
 				<td class="center"><?=$i?></td>
 				<td><?=($row['level'] > 0) ? "|".str_repeat("__", $row['level']): ""?> <?=(strlen($row['title']) > 20? substr($row['title'], 0,20) . '...': $row['title'])?></td>
-				<td><?=$row['summary']?></td>
+				<td><a href='<?php echo site_url('news/cat/' . $row['uri']) ?>' target='_blank'>news/cat/<?=$row['uri']?></a></td>
 				<td><?php if ($row['status']==1): echo 'Active'; else: echo 'Suspended'; endif;?></td>
 				<td>
 				<a href="<?=site_url('admin/news/category/move/up/'. $row['id'])?>"><img src="<?=site_url('application/views/admin/images/moveup.gif')?>" width="16" height="16" title="<?=__("Move up", $this->template['module'])?>" alt="<?=__("Move up", $this->template['module'])?>"/></a>
