243f1574671ca1c71edb1c21cb83f4032558bc19
heriniaina.eugene <heriniaina.eugene@23e6ee1f-784e-0410-b58e-b1cedd78a7fc>
2010-10-14 07:50:39 +0000

    + Added print_menu_by_title to print menu structure by title done by rmorgan http://code.google.com/p/ci-cms/issues/detail?id=48 ! Fixed bug on _print_menu. Didn't escape

--- application/libraries/Navigation.php
+++ application/libraries/Navigation.php
@@ -124,10 +124,16 @@
 		function _print_menu ($parent = 0)
 		{
 
-			$this->obj->db->where(array('parent_id' => $parent, 'lang' => $this->obj->user->lang, 'active' => 1));
-			$this->obj->db->where(array('g_id IN' => "('" . join("', '" , $this->obj->user->groups) . "')"));
-			$this->obj->db->orderby('parent_id, weight');
-			$query = $this->obj->db->get('navigation');		
+
+			$select = "SELECT * FROM " . $this->obj->db->dbprefix('navigation')
+			. " WHERE parent_id = '$parent' "
+			. " AND lang = '" . $this->obj->user->lang . "' "
+			. " AND `active` = 1 " 
+			. " AND g_id IN ('".join("', '", $this->obj->user->groups)."') "
+			. " ORDER BY `parent_id`, `weight`";
+			$query = $this->obj->db->query($select);
+
+
 		 	if ($query->num_rows() == 0)
 			{
 				return false;
@@ -158,6 +164,60 @@
 		}
 		
 
+		/*******
+		 *
+		 * thankx to rmorgan
+		 * http://code.google.com/p/ci-cms/issues/detail?id=48
+		 * Use: echo $this->navigation->print_menu_by_title('Footer-Menu');
+		 *
+		 ******/
+		 
+	   function print_menu_by_title($title='')
+	   {
+			   //echo  "Parent Id: ".$parent;
+					   
+			   if (!$data = $this->obj->cache->get('navigation'.$title.$this->obj->user->lang, 'navigation'))
+			   {
+					   $data = $this->_print_menu_by_title($title);
+					   $this->obj->cache->save('navigation'.$title.$this->obj->user->lang, $data, 'navigation',0 );
+			   }
+			   return $data;
+	   }
+
+	   function _print_menu_by_title($title='')
+	   {
+			   $select = "SELECT t1.* "
+					   ."FROM " . $this->obj->db->dbprefix('navigation') . " t1 "
+					   ."JOIN " . $this->obj->db->dbprefix('navigation') . " t2 ON t1.parent_id = t2.id "
+					   ."WHERE t2.title = '$title' "
+					   ."AND t1.active = 1 "
+					   ."AND t2.lang = '".$this->obj->user->lang."' "
+					   ."AND t1.lang = '".$this->obj->user->lang."' "
+					   ."AND (t2.g_id IN ('".join("', '", $this->obj->user->groups)."')) "
+					   ."AND (t1.g_id IN ('".join("', '", $this->obj->user->groups)."')) "
+					   ."ORDER BY t1.parent_id, t1.weight";
+					   
+	   
+			   $query = $this->obj->db->query($select);                
+			   
+			   if ($query->num_rows() == 0)
+			   {
+					   return false;
+			   }
+			   $html = '<ul ' . (($this->nav_opened == false) ? 'class="nav"' : '') . '>';
+			   $this->nav_opened = true;
+			   foreach ($query->result_array() as $item)
+			   {
+					   $html .= '<li ' . ($item['uri'] == substr($this->obj->uri->uri_string(), 1) ? 'class="current" ' : '') . '>';
+					   $html .= '<a href="'. $item['uri'] .'">'.$item['title'].'</a>';
+					   // calling same function where we are (recursive) to check if menu item has submenu
+					   $html .= $this->_print_menu($item['id']);
+					   $html .= '</li>';
+			   }
+	   
+			   $html .= '</ul>';
+			   return $html;
+	   }               		
 	}
 
 
